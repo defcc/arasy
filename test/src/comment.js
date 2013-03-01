e37efa156098afd14e2866e4186fd923bf2650a6
urlinvalid <chengchao1@gmail.com>
2013-03-01 16:34:37 +0800

    update test case: comment

--- test/src/comment.js
+++ test/src/comment.js
@@ -7,4 +7,11 @@ test('注释的解析', function(){
     var tokens1 = YAP(source1).parse();
     ok(tokens1.length == 1, '结尾slash被转义，最终1个token');
 
+    //nested comment
+
+    var nestSource = '/*/***/**/';
+    var nestTokens = YAP(nestSource).parse();
+    console.log(nestTokens);
+    ok(nestTokens.length == 4, '4个token');
+    equal(nestTokens.length, 4, '')
 });
