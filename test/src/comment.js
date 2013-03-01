4bbaa5170fbd3b92cd21aae4cc072c7408c15a0c
urlinvalid <chengchao1@gmail.com>
2013-03-01 13:17:39 +0800

    update test case

--- test/src/comment.js
+++ test/src/comment.js
@@ -2,4 +2,9 @@ test('注释的解析', function(){
     var source = '/**/var';
     var tokens = YAP(source).parse();
     ok(tokens.length == 2, '2个token');
+
+    var source1 = '/**\\/incomment';
+    var tokens1 = YAP(source1).parse();
+    ok(tokens1.length == 1, '结尾slash被转义，最终1个token');
+
 });
