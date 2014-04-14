4e5583754902520217f0fb4bdcfb94eb15116dfc
defcc <chengchao1@gmail.com>
2014-04-14 17:02:44 +0800

    [后面是 regexp start

--- arasy/expressions/arrayExpression.js
+++ arasy/expressions/arrayExpression.js
@@ -3,7 +3,7 @@ var arrayParser = function( expressionParser, token ){
 
     var nextToken = expressionParser.scanner.lookAhead();
     if ( nextToken.value != ']' ) {
-        rs = expressionParser.parse(0);
+        rs = expressionParser.parse(0, 0, 0, 'prefix');
     }
     //must be right bracket
     expressionParser.scanner.nextToken();
