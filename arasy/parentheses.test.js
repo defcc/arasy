acbdaa12a825d9abaad8d7fbadf6e640647297d6
defcc <chengchao1@gmail.com>
2014-04-19 18:12:52 +0800

    update parentheses cases & slash cases

--- arasy/parentheses.test.js
+++ arasy/parentheses.test.js
@@ -1,7 +1,7 @@
 // paran 有两种语义。一种是 group operator， 一种是 call operator
 
 // statement 开始时的 paran 是 group operator
-if( true ) (a||b)();
+if( true ) (a||b);
 
 // operator 后面是  group operator
 // void (a)
@@ -10,4 +10,6 @@ if( true ) (a||b)();
 
 // 可以结合 slash来一起处理
 
+
+// 结论
 // 如果当前 允许 正则 的话， 那么就是 group operator ，否则是 call operator
