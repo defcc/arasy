dc218fb3ce3796c569c375b5e988037402364c0a
defcc <chengchao1@gmail.com>
2014-04-19 20:24:00 +0800

    conditional 的 consequentPart 不允许出现comma

--- arasy/expressions/conditionalExpression.js
+++ arasy/expressions/conditionalExpression.js
@@ -1,12 +1,12 @@
-var conditionalOperatorParser = function ( expressionParser, left, token ) {
+var conditionalOperatorParser = function ( expressionParser, left, token, noComma ) {
     var rs = {
         type: 'ConditionalExpression'
     };
     var conditionalOpPrecedence = getPrecedenceByToken( token );
     rs.left = left;
-    rs.consequentPart = expressionParser.parse( 0 );
+    rs.consequentPart = expressionParser.parse( 0, 1 );
     expectValue(':', expressionParser.scanner.nextToken());
-    rs.alternate = expressionParser.parse( 0 );
+    rs.alternate = expressionParser.parse( 0, noComma );
     return rs;
 };
 
